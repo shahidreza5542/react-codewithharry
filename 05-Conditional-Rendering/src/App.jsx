6579debaa3378d95c2e8e706d5ed5fa948e61933
shahidreza <shahidreza660@gmail.com>
2025-08-27 09:21:24 +0530

    create new vite project

--- 05-Conditional-Rendering/src/App.jsx
+++ 05-Conditional-Rendering/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showbtn, setshowbtn] = useState(false)
+  const changeref = useRef()
+  useEffect(() => {
+    setshowbtn(true)
+    changeref.current.style.backgroundColor = "blue"
+  }, [])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+   const [todos, setTodos] = useState([
+    {
+      title: "Hey",
+      desc: "I am a good todo"
+    },
+    {
+      title: "Hey Another todo",
+      desc: "I am a good todo too"
+    },
+    {
+      title: "Hey I am grocery todo",
+      desc: "I am a good todo but I am grocery todo"
+    },
+
+  ])
+
+  const Todo = ({todo}) => {
+    return (<>
+    <div className="m-4 border border-1 border-purple-400">
+
+      <div className="todo">{todo.title}</div>
+      <div className="todo">{todo.desc}</div>
+    </div>
+      </>)
+  }
+ 
+
+
+
+
+return (
+  <>
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+    <h1>Vite + React</h1>
+    {showbtn ? <button>show btn is true</button> : "show btn is false"}
+    {/* {showbtn && <button>i will be shown when second buttion is clicked</button>} */}
+
+
+    {todos.map(todo=>{
+      return <Todo todo={todo}/>
+    })}
+
+
+
+    <div className="card">
+      <button ref={changeref} onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
       </p>
-    </>
-  )
+    </div>
+    <p className="read-the-docs">
+      Click on the Vite and React logos to learn more
+    </p>
+  </>
+)
 }
 
 export default App
---
+++ 06-handling-events/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState("shahid")
+  // const [name, setname] = useState("shahid")
+  const [form, setform] = useState({name: "name",phone: "phone"})
+  
+  const handleclick = () => {
+    alert("clicked buttion")
+  }
+
+  const handlemouseover = () => {
+  }
+
+  const handlevalue = (e) => {
+    setform(e.target.value);
+    
+    
+  }
+
+  
+  
+
+
+  return (
+    <>
+      <div className="button">
+        <button onClick={handleclick}>click me</button>
+      </div>
+      <div onMouseOver={handlemouseover} className="red">red</div>
+      <input type="email" value={form.name} onChange={handlevalue} />
+      <input type="phone" value={form.phone} onChange={handlevalue} />
+    </>
+  )
+}
+
+export default App
